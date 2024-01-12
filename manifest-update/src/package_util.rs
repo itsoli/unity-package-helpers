9d18e3d3f3022046767df3528235f9f7ca98f7e4
Oliver Paschke <oliver.paschke@bytro.com>
2024-01-13 00:36:34 +0100

    Add find package iterator

--- manifest-update/src/package_util.rs
+++ manifest-update/src/package_util.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
-use std::path::PathBuf;
+use std::iter::{Filter, Flatten};
+use std::path::{Path, PathBuf};
 
-// use jwalk::WalkDir;
+use jwalk::WalkDir;
+// use rayon::iter::{Filter, Flatten};
 // use rayon::prelude::*;
 use serde::Deserialize;
 // use walkdir::WalkDir;
@@ -15,17 +17,210 @@ pub struct Package {
     pub version: Version,
 }
 
+// type IteratorType = Filter<Flatten<jwalk::DirEntryIter<((), ())>>, dyn FnMut(&jwalk::DirEntry<((), ())>) -> bool>;
+// type TheType = Filter<Flatten<jwalk::DirEntryIter<((), ())>>, impl FnMut(&jwalk::DirEntry<((), ())>) -> bool>;
+
+// struct FindPackages<F: FnMut(&jwalk::DirEntry<((), ())>) -> bool> {
+// struct FindPackages<F: FnMut(&jwalk::DirEntry<((), ())>) -> bool + Sized> {
 // struct FindPackages {
-//     it: <walkdir::WalkDir as std::iter::IntoIterator>::IntoIter,
+// struct FindPackages<T> {
+//     // it: <walkdir::WalkDir as std::iter::IntoIterator>::IntoIter,
 //     // it: u32,
+//     // walk_dir: jwalk::WalkDirGeneric<((), ())>,
+//     // it: Filter<>,
+//     // it: Filter<Flatten<jwalk::DirEntryIter<((), ())>>, dyn Fn(&jwalk::DirEntry<((), ())>) -> bool>,
+//     // it: Filter<I, P>,
+//     // it: Filter<Flatten<jwalk::DirEntryIter<((), ())>>, F>,
+//     // it: IteratorType,
+//     // it: Filter<Flatten<jwalk::DirEntryIter<((), ())>>, dyn FnMut(&jwalk::DirEntry<((), ())>) -> bool + Sized>,
+//     it: T,
 // }
 
-// impl FindPackages {
-//     pub fn new<P: AsRef<Path>>(packages_path: P) -> Self {
-//         Self {
-//             it: WalkDir::new(packages_path).into_iter(),
+// struct FindPackages<I: Iterator<Item = jwalk::DirEntry<((), ())>>> {
+//     it: I,
+// }
+
+// impl<I: Iterator, P> Iterator for Filter<I, P>
+// where
+//     P: FnMut(&I::Item) -> bool,
+
+// struct FindPackages<P: FnMut(&jwalk::DirEntryIter<((), ())>) -> bool> {
+//     it: Filter<Flatten<jwalk::DirEntryIter<((), ())>>, P>,
+// }
+
+struct FindPackages {
+    it: Flatten<jwalk::DirEntryIter<((), ())>>,
+}
+
+// impl<F: FnMut(&jwalk::DirEntry<((), ())>) -> bool> FindPackages<F> {
+// impl<F: FnMut(&jwalk::DirEntry<((), ())>) -> bool + Sized> FindPackages<F> {
+impl FindPackages {
+// impl<T> FindPackages<T> {
+// impl<I: Iterator<Item = jwalk::DirEntry<((), ())>>> FindPackages<I> {
+// impl<I: Iterator, P: FnMut(&I::Item) -> bool> FindPackages<I, P> {
+// impl<P: FnMut(&jwalk::DirEntryIter<((), ())>) -> bool> FindPackages<P> {
+    // pub fn new<PP: AsRef<Path>>(packages_path: PP) -> FindPackages<Filter<Flatten<jwalk::DirEntryIter<((), ())>>, impl FnMut(&jwalk::DirEntry<((), ())>) -> bool>> {
+    // pub fn new<PP: AsRef<Path>>(packages_path: PP) -> FindPackages<impl FnMut(&'a jwalk::DirEntryIter<((), ())>) -> bool + '_> {
+    pub fn new<PP: AsRef<Path>>(packages_path: PP) -> FindPackages {
+        let walk_dir = WalkDir::new(packages_path).process_read_dir(|depth, path, read_dir_state, children| {
+            let is_package = children.iter().any(|dir_entry_result| {
+                dir_entry_result
+                    .as_ref()
+                    .map(|dir_entry| dir_entry.file_type.is_file() && dir_entry.file_name == "package.json")
+                    .unwrap_or(false)
+            });
+            if is_package {
+                children.retain(|dir_entry_result| {
+                    dir_entry_result
+                        .as_ref()
+                        .map(|dir_entry| dir_entry.file_type.is_file() && dir_entry.file_name == "package.json")
+                        .unwrap_or(false)
+                });
+            } else {
+                children.retain(|dir_entry_result| {
+                    dir_entry_result
+                        .as_ref()
+                        .map(|dir_entry| dir_entry.file_type.is_dir())
+                        .unwrap_or(false)
+                });
+            }
+
+            // let package = children.iter().find_map(|dir_entry_result| {
+            //     dir_entry_result.as_ref().map_or(None, |dir_entry| {
+            //         if dir_entry.file_type.is_file() && dir_entry.file_name == "package.json" {
+            //             Some(dir_entry)
+            //         } else {
+            //             None
+            //         }
+            //     })
+            // });
+
+            // children.retain(|dir_entry_result| {
+            //     dir_entry_result.as_ref().map(|dir_entry| {
+            //         dir_entry.file_type.is_dir() || dir_entry.file_name == "package.json"
+            //         // dir_entry.file_name
+            //         //     .to_str()
+            //         //     .map(|s| s == "package.json")
+            //         //     .unwrap_or(false)
+            //     }).unwrap_or(false)
+            // });
+        });
+
+        use jwalk::*;
+
+        // Filter<Flatten<jwalk::DirEntryIter<((), ())>>, FnMut(&Self::Item) -> bool>
+        // let it = walk_dir
+        //     .into_iter()
+        //     .flatten()
+        //     .filter(|entry| entry.file_type.is_file());
+        let it = walk_dir.into_iter().flatten();
+
+        // while let Some(entry) = it.next() {
+        //     let reader = open_reader(entry.path()).unwrap();
+        //     let package: Package = serde_json::from_reader(reader).unwrap();
+        //     // (package.name, package.version)
+        // }
+
+            // it.map(|entry| {
+            //     let reader = open_reader(entry.path()).unwrap();
+            //     let package: Package = serde_json::from_reader(reader).unwrap();
+            //     (package.name, package.version)
+            // })
+            // .collect::<HashMap::<String, Version>>();
+
+        FindPackages {
+            // it: WalkDir::new(packages_path).into_iter(),
+            // walk_dir,
+            it,
+        }
+    }
+}
+
+impl Iterator for FindPackages {
+    type Item = Package;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for entry in self.it.by_ref() {
+            if entry.file_type.is_file() {
+                let reader = open_reader(entry.path()).unwrap();
+                let result: serde_json::Result<Package> = serde_json::from_reader(reader);
+                if let Ok(package) = result {
+                    return Some(package);
+                }
+            }
+        }
+        None
+    }
+}
+
+// impl Iterator for FindPackages<Filter<Flatten<jwalk::DirEntryIter<((), ())>>, impl FnMut(&jwalk::DirEntry<((), ())>) -> bool>> {
+// impl<T: Iterator<Item = PathBuf>> Iterator for FindPackages<T> {
+// impl<I: Iterator<Item = jwalk::DirEntry<((), ())>>> Iterator for FindPackages<I> {
+// impl<I: Iterator, P: FnMut(&I::Item) -> bool> Iterator for FindPackages<I, P> {
+// impl<T: Filter<Flatten<jwalk::DirEntryIter<((), ())>>, impl FnMut(&jwalk::DirEntry<((), ())>) -> bool>> Iterator for FindPackages<T> {
+// impl<P: FnMut(&jwalk::DirEntryIter<((), ())>) -> bool> Iterator for FindPackages<P> {
+//     // type Item = PathBuf;
+//     type Item = (String, Version);
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         // if let Some(entry) = self.it.next() {
+//         //     // let reader = open_reader(entry.path()).unwrap();
+//         //     let reader = open_reader(entry.as_path()).unwrap();
+//         //     let package: Package = serde_json::from_reader(reader).unwrap();
+//         //     return Some((package.name, package.version));
+//         // }
+//         // None
+
+//         match self.it.next() {
+//             Some(entry) => {
+//                 // let reader = open_reader(entry.path()).unwrap();
+//                 let reader = open_reader(entry.path()).unwrap();
+//                 let package: Package = serde_json::from_reader(reader).unwrap();
+//                 Some((package.name, package.version))
+//             },
+//             None => None,
 //         }
+
+//         // loop {
+//         //     let entry = match self.it.next() {
+//         //         None|Some(Err(_)) => return None,
+//         //         Some(Ok(entry)) => entry,
+//         //     };
+
+//         //     if !entry.file_type().is_file() {
+//         //         continue;
+//         //     }
+//         //     if entry.file_name() != "package.json" {
+//         //         continue;
+//         //     }
+
+//         //     self.it.skip_current_dir();
+
+//         //     return Some(entry.into_path());
+//         // }
 //     }
+// }
+
+// impl<T: Iterator<Item = PathBuf>> IntoIterator for FindPackages<T> {
+//     type Item = (String, Version);
+//     type IntoIter = FindPackages<T>;
+
+//     fn into_iter(self) -> FindPackages<T> {
+//         self
+//     }
+// }
+
+fn find_packages_x(packages_path: &str) -> FindPackages {
+    // let x = FindPackages::new(packages_path);
+    // for i in x {
+    // }
+    FindPackages::new(packages_path)
+}
+
+
+// fn find_packages<P: AsRef<Path>>(packages_path: P) -> impl Iterator<Item = (String, Version)> {
+//     let fp = FindPackages::new(packages_path);
+//     fp.n
 // }
 
 // impl Iterator for FindPackages {
@@ -215,10 +410,17 @@ fn impl_jwalk(packages_path: &str) -> HashMap::<String, Version> {
         .collect::<HashMap::<String, Version>>()
 }
 
+fn impl_jwalk_it(packages_path: &str) -> HashMap::<String, Version> {
+    find_packages_x(packages_path)
+        .map(|package| (package.name, package.version))
+        .collect::<HashMap::<String, Version>>()
+}
+
 pub fn find_packages(packages_path: &str) -> Result<HashMap::<String, Version>> {
     // impl_sync(packages_path) // 0.060
     // Ok(impl_rayon(packages_path)) // 0.058
-    Ok(impl_jwalk(packages_path)) // 0.040
+    // Ok(impl_jwalk(packages_path)) // 0.040
+    Ok(impl_jwalk_it(packages_path))
 }
 
 pub fn get_sorted_package_list(packages: &HashMap::<String, Version>) -> Vec<Package> {
