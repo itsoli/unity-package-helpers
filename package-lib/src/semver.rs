7a818f12f70fb5a68d2bc960b4742cea847e425a
Oliver Paschke <oliver.paschke@bytro.com>
2024-01-14 22:58:44 +0100

    Use manual version parsing

--- package-lib/src/semver.rs
+++ package-lib/src/semver.rs
@@ -2,35 +2,63 @@ use std::error;
 use std::fmt;
 use std::str::FromStr;
 
-use lazy_static::lazy_static;
-use regex::Regex;
 use serde::de::{self, Visitor};
 use serde::{Deserialize, Deserializer};
 
 /// Error parsing a semantic version.
 #[derive(Debug)]
 pub enum VersionError {
-    InvalidVersionString,
-    MajorOutOfRange,
-    MinorOutOfRange,
-    PatchOutOfRange,
+    Empty,
+    UnexpectedEnd(Position),
+    UnexpectedChar(Position, char),
+    UnexpectedCharAfter(Position, char),
+    LeadingZero(Position),
+    Overflow(Position),
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub enum Position {
+    Major,
+    Minor,
+    Patch,
 }
 
 impl error::Error for VersionError {}
 
 impl fmt::Display for VersionError {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         use self::VersionError::*;
         match self {
-            InvalidVersionString => write!(fmt, "Invalid version string"),
-            MajorOutOfRange => write!(fmt, "Major version out of u16 range"),
-            MinorOutOfRange => write!(fmt, "Minor version out of u16 range"),
-            PatchOutOfRange => write!(fmt, "Patch version out of u16 range"),
+            Empty => write!(formatter, "Empty version string"),
+            UnexpectedEnd(pos) => {
+                write!(formatter, "Unexpected end of input while parsing {}", pos)
+            }
+            UnexpectedChar(pos, ch) => write!(
+                formatter,
+                "Unexpected character '{}' while parsing {}",
+                *ch, pos
+            ),
+            UnexpectedCharAfter(pos, ch) => {
+                write!(formatter, "Unexpected character '{}' after {}", *ch, pos)
+            }
+            LeadingZero(pos) => write!(formatter, "Leading zero while parsing {}", pos),
+            Overflow(pos) => write!(formatter, "Value of {} exceeds u16::MAX", pos),
         }
     }
 }
 
-/// Semantic version.
+impl fmt::Display for Position {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        use self::Position::*;
+        formatter.write_str(match self {
+            Major => "major version number",
+            Minor => "minor version number",
+            Patch => "patch version number",
+        })
+    }
+}
+
+/// Semantic version. Does not support pre-release or build metadata.
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug)]
 pub struct Version {
     pub major: u16,
@@ -67,36 +95,81 @@ impl Version {
     }
 }
 
+impl fmt::Display for Version {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{}.{}.{}", self.major, self.minor, self.patch)
+    }
+}
+
 impl FromStr for Version {
     type Err = VersionError;
 
     /// Parses a semantic version from a string.
     fn from_str(value: &str) -> Result<Self, Self::Err> {
-        lazy_static! {
-            static ref VERSION_PATTERN: Regex =
-                Regex::new(r"([0-9]|[1-9][0-9]+)\.([0-9]|[1-9][0-9]+)\.([0-9]|[1-9][0-9]+)")
-                    .unwrap();
+        if value.is_empty() {
+            return Err(VersionError::Empty);
         }
-        let captures = VERSION_PATTERN
-            .captures(value)
-            .ok_or(VersionError::InvalidVersionString)?;
+
+        let mut pos = Position::Major;
+        let (major, value) = parse_number(value, pos)?;
+        let value = parse_dot(value, pos)?;
+
+        pos = Position::Minor;
+        let (minor, value) = parse_number(value, pos)?;
+        let value = parse_dot(value, pos)?;
+
+        pos = Position::Patch;
+        let (patch, value) = parse_number(value, pos)?;
+
+        if let Some(unexpected) = value.chars().next() {
+            return Err(VersionError::UnexpectedCharAfter(pos, unexpected));
+        }
+
         Ok(Version {
-            major: captures[1]
-                .parse::<u16>()
-                .map_err(|_| VersionError::MajorOutOfRange)?,
-            minor: captures[2]
-                .parse::<u16>()
-                .map_err(|_| VersionError::MinorOutOfRange)?,
-            patch: captures[3]
-                .parse::<u16>()
-                .map_err(|_| VersionError::PatchOutOfRange)?,
+            major,
+            minor,
+            patch,
         })
     }
 }
 
-impl fmt::Display for Version {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(fmt, "{}.{}.{}", self.major, self.minor, self.patch)
+fn parse_number(input: &str, pos: Position) -> Result<(u16, &str), VersionError> {
+    let mut len = 0;
+    let mut value = 0u16;
+
+    while let Some(&digit) = input.as_bytes().get(len) {
+        if !digit.is_ascii_digit() {
+            break;
+        }
+        if value == 0 && len > 0 {
+            return Err(VersionError::LeadingZero(pos));
+        }
+        match value
+            .checked_mul(10)
+            .and_then(|value| value.checked_add((digit - b'0') as u16))
+        {
+            Some(sum) => value = sum,
+            None => return Err(VersionError::Overflow(pos)),
+        }
+        len += 1;
+    }
+
+    if len > 0 {
+        Ok((value, &input[len..]))
+    } else if let Some(unexpected) = input[len..].chars().next() {
+        Err(VersionError::UnexpectedChar(pos, unexpected))
+    } else {
+        Err(VersionError::UnexpectedEnd(pos))
+    }
+}
+
+fn parse_dot(input: &str, pos: Position) -> Result<&str, VersionError> {
+    if let Some(rest) = input.strip_prefix('.') {
+        Ok(rest)
+    } else if let Some(unexpected) = input.chars().next() {
+        Err(VersionError::UnexpectedChar(pos, unexpected))
+    } else {
+        Err(VersionError::UnexpectedEnd(pos))
     }
 }
 
